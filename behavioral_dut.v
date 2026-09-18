0f13988ef0791ca09f69ae7498e913c6f2e827f7
Avronil <basuavronil65@gmail.com>
2026-09-18 22:29:47 +0530

    Update behavioral_dut.v

--- behavioral_dut.v
+++ behavioral_dut.v
@@ -1,25 +1,32 @@
-module barrel_shifter (
-    input  wire [3:0] data_in,   // Data input
-    input  wire [1:0] shift_amt, // Shift amount (0-3 bits)
-    input  wire [1:0] mode,      // 00: LSL, 01: LSR, 10: ASR, 11: ROR
-    output reg  [3:0] data_out   // Data output
+module barrale_shifter(
+  input clk, rst,
+  input [3:0] d_in,
+  input [1:0] mode,
+  input [1:0] shift_amt,
+  output [3:0] d_out
 );
-
-    always @(*) begin
+  reg [3:0] mem;
+  always@(posedge clk or negedge rst)
+    begin
+      if (!rst)
+        mem <= 4'd0;
+      else begin
         case (mode)
-            2'b00: data_out = data_in << shift_amt;                  // Logical Left Shift
-            2'b01: data_out = data_in >> shift_amt;                  // Logical Right Shift
-            2'b10: data_out = $signed(data_in) >>> shift_amt;        // Arithmetic Right Shift
-            2'b11: begin                                             // Rotate Right
-                case (shift_amt)
-                    2'b00: data_out =  data_in;
-                    2'b01: data_out = {data_in[0],   data_in[3:1]};
-                    2'b10: data_out = {data_in[1:0], data_in[3:2]};
-                    2'b11: data_out = {data_in[2:0], data_in[3]};
-                endcase
-            end
-            default: data_out = data_in;
+          2'd00: mem = d_in << shift_amount;
+          2'd01: mem = d_in >> shift_amount;
+          2'b10; mem = $signed(d_in) >>> shift_amount; // retaining the signed bit
+          2'b11: begin // rotate right
+            case (shift_amount)
+                2'b00: ;
+                2'b01: mem = {mem[0], mem[3:1]}; 
+                2'b10: mem = {mem[1:0], mem[3:2]};
+                2'b11: mem = {mem[2:0], mem[3]};
+                default: mem = mem;
+            endcase
+          end
+          default: ;
         endcase
+      end
+      
     end
-
-endmodule
+endmodule 
